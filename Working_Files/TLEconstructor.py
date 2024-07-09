87dfaa96f9daa2eb0e4d451d378b5b3cca862131
AidenKriel <aidenkriel379@gmail.com>
2024-07-09 14:36:38 -0400

    Slight updates to orbit code

--- Working_Files/TLEconstructor.py
+++ Working_Files/TLEconstructor.py
@@ -6,6 +6,7 @@ import pandas as pd
 from pytz import timezone
 
 start_time = time.time()
+print("Simulating Sattelite Orbit...")
 # Load timescale
 ts = load.timescale()
 eastern = timezone('US/Eastern')
