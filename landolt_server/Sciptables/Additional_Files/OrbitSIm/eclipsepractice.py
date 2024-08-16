a424b665f6a817efa39bf7133340955003a30c06
AidenKriel <aidenkriel379@gmail.com>
2024-08-16 19:05:53 -0400

    Updated Eclipse Code

--- landolt_server/Sciptables/Additional_Files/OrbitSIm/eclipsepractice.py
+++ landolt_server/Sciptables/Additional_Files/OrbitSIm/eclipsepractice.py
@@ -1,12 +1,13 @@
 from sgp4.api import Satrec
 from skyfield.api import load, EarthSatellite, wgs84
 from skyfield.framelib import itrs
+from skyfield.positionlib import Barycentric
 
 
 # Load timescale
 ts = load.timescale()
 eph = load('de421.bsp')
-sun = eph['sun']
+sun, earth = eph['sun'], eph['earth']
 
 s = '1 25544U 98067A   24229.50766309  .00023344  00000-0  41274-3 0  9992'
 t = '2 25544  51.6410  16.4966 0005341 207.2907 263.9809 15.50198988467908'
@@ -23,7 +24,8 @@ difference = sat - obs
 
 t = ts.now()
 
-satpos = sat.at(t)
-sunpos = sun.at(t)
-print(sunpos.frame)
+satpos = Barycentric(sat.at(t).position.au)
+print(satpos)
+sunpos = sat.at(t).observe(sun)
+print(sunpos.position)
 print(satpos.position)
