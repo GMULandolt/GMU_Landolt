6eb8662042e225959c8ce08e07a1e492b33e45e3
Elizabeth Lillehei <0lillehei@gmail.com>
2024-08-22 17:48:28 -0400

    minor changes

--- science_scripts/Working_Files/Image_sim.py
+++ science_scripts/Working_Files/Image_sim.py
@@ -38,13 +38,7 @@ height    = 1024                   #Pixel image sizes
 width     = 1024
 pixel     = 0.0002777777778     # [deg] Coordinate increment at reference point. 0.0002777777778 corresponds to 1px/arcsecond
 
-
-#--------------------------------------------------------------------------------------------------------------------
-# FUNCTIONS
-#--------------------------------------------------------------------------------------------------------------------
-
-# 15 "/sec / 0.34 "/px = 44 px/s
-# 3 "/sec / 0.34 "/px = 9 px/s
+# Multiplying this number by 12.5 nets us 3.555556, or 1px/ 12.5 arcseconds, This will be the largest number we will use
 
 #--------------------------------------------------------------------------------------------------------------------
 # SPACECRAFT DATA 
@@ -62,8 +56,6 @@ R_Flux  = data[:,6]
 # Count   = data[:,7]
 # AirMass = data[:,8]
 
-print(RA)
-print(Dec)
 
 def find_middle(lst):
     length = len(lst)  # Get the length of the list
@@ -80,7 +72,8 @@ def find_middle(lst):
 RA_import = find_middle(RA)
 Dec_import = find_middle(Dec)
 
-print(len(data))
+print("The length of the imported CSV File is {}".format(len(data)))
+print("If the CSV file is over 10,000 you may encounter long wait times \n")
 
 print ("INFO: SPACECRAFT DATA IMPORTED")
 #--------------------------------------------------------------------------------------------------------------------
@@ -117,9 +110,211 @@ print ("INFO: SPACECRAFT DATA IMPORTED")
 # HIPS2FITS QUERY FOR FITS FILE
 #--------------------------------------------------------------------------------------------------------------------
 
+print("Your Imported RA is {} \n".format(abs(RA[-1]-RA[0])))
 
+if abs(RA[-1]-RA[0]) >= 3.55:
+    print("RA Tracking too large! Please limit the scale of the data to under 3.55 degrees")
+elif 2.84  <= abs(RA[-1]-RA[0]) < 3.55:
+    w = astropy_wcs.WCS(header={
+	'BITPIX': 16,
+    'WCSAXES': 2,           # Number of coordinate axes
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 12.5,        
+    'CRPIX1': 512, 
+    'CRVAL1': RA_import,
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 12.5,      
+    'CRPIX2': 512, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    })
 
-w = astropy_wcs.WCS(header={
+    wcs_input_dict = {
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 12.5, 
+    'CRPIX1': 1, 
+    'CRVAL1': RA_import, 
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 12.5, 
+    'CRPIX2': 1, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    }
+    print("For this image, 1px is 12.5 arcseconds")
+elif 2.27  <= abs(RA[-1]-RA[0]) < 2.84:
+    w = astropy_wcs.WCS(header={
+	'BITPIX': 16,
+    'WCSAXES': 2,           # Number of coordinate axes
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 10,        
+    'CRPIX1': 512, 
+    'CRVAL1': RA_import,
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 10,      
+    'CRPIX2': 512, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    })
+    wcs_input_dict = {
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 10, 
+    'CRPIX1': 1, 
+    'CRVAL1': RA_import, 
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 10, 
+    'CRPIX2': 1, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    }
+
+    print("For this image, 1px is 10 arcseconds")
+elif 1.70  <= abs(RA[-1]-RA[0]) < 2.27:
+    w = astropy_wcs.WCS(header={
+	'BITPIX': 16,
+    'WCSAXES': 2,           # Number of coordinate axes
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 8,        
+    'CRPIX1': 512, 
+    'CRVAL1': RA_import,
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 8,      
+    'CRPIX2': 512, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    })
+    wcs_input_dict = {
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 8, 
+    'CRPIX1': 1, 
+    'CRVAL1': RA_import, 
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 8, 
+    'CRPIX2': 1, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    }
+
+    print("For this image, 1px is 8 arcseconds")
+elif 1.13  <= abs(RA[-1]-RA[0]) < 1.70:
+    w = astropy_wcs.WCS(header={
+	'BITPIX': 16,
+    'WCSAXES': 2,           # Number of coordinate axes
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 6,        
+    'CRPIX1': 512, 
+    'CRVAL1': RA_import,
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 6,      
+    'CRPIX2': 512, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    })
+    wcs_input_dict = {
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 6, 
+    'CRPIX1': 1, 
+    'CRVAL1': RA_import, 
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 6, 
+    'CRPIX2': 1, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    }
+
+    print("For this image, 1px is 6 arcseconds")
+elif 0.56  <= abs(RA[-1]-RA[0]) < 1.13:
+    w = astropy_wcs.WCS(header={
+	'BITPIX': 16,
+    'WCSAXES': 2,           # Number of coordinate axes
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 4,        
+    'CRPIX1': 512, 
+    'CRVAL1': RA_import,
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 4,      
+    'CRPIX2': 512, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    })
+    wcs_input_dict = {
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 4, 
+    'CRPIX1': 1, 
+    'CRVAL1': RA_import, 
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 4, 
+    'CRPIX2': 1, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    }
+
+    print("For this image, 1px is 4 arcseconds")
+elif 0.28  <= abs(RA[-1]-RA[0]) < 0.56:
+    w = astropy_wcs.WCS(header={
+	'BITPIX': 16,
+    'WCSAXES': 2,           # Number of coordinate axes
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 2,        
+    'CRPIX1': 512, 
+    'CRVAL1': RA_import,
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 2,      
+    'CRPIX2': 512, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    })
+
+    wcs_input_dict = {
+    'CTYPE1': 'RA---TAN', 
+    'CUNIT1': 'deg', 
+    'CDELT1': pixel * 2, 
+    'CRPIX1': 1, 
+    'CRVAL1': RA_import, 
+    'NAXIS1': height,
+    'CTYPE2': 'DEC--TAN', 
+    'CUNIT2': 'deg', 
+    'CDELT2': pixel * 2, 
+    'CRPIX2': 1, 
+    'CRVAL2': Dec_import, 
+    'NAXIS2': width
+    }
+
+    print("For this image, 1px is 2 arcseconds")
+elif 0.001 <= abs(RA[-1]-RA[0]) < 0.28:
+    w = astropy_wcs.WCS(header={
 	'BITPIX': 16,
     'WCSAXES': 2,           # Number of coordinate axes
     'CTYPE1': 'RA---TAN', 
@@ -134,10 +329,8 @@ w = astropy_wcs.WCS(header={
     'CRPIX2': 512, 
     'CRVAL2': Dec_import, 
     'NAXIS2': width
-})
-
-
-wcs_input_dict = {
+    })
+    wcs_input_dict = {
     'CTYPE1': 'RA---TAN', 
     'CUNIT1': 'deg', 
     'CDELT1': pixel, 
@@ -150,7 +343,10 @@ wcs_input_dict = {
     'CRPIX2': 1, 
     'CRVAL2': Dec_import, 
     'NAXIS2': width
-}
+    }
+    print("For this image, 1px is 1 arcsecond")
+elif 0.00  <= abs(RA[-1]-RA[0]) < 0.001:
+    print("RA Tracking too small! Please limit the scale of the date to over 0.001 degrees.") 
 
 print ("INFO: WCS Header Created")
 
@@ -158,7 +354,6 @@ result = hips2fits.query_with_wcs(
     hips=Database,
     wcs = w,
     get_query_payload=False,
-   
 )
 
 print ("INFO: Fits file imported")
@@ -180,7 +375,10 @@ wcs_landolt_dict = astropy_wcs.WCS(wcs_input_dict)
 
 header_data_unit_list = fits.open("2MASS_FITS.fits")
 
+print("Displaying Basic FITS header data")
 header_data_unit_list.info()
+
+print()
 
 image1 = header_data_unit_list[0].data
 
@@ -196,14 +394,7 @@ wcs_landolt = astropy_wcs.WCS(header1)
 #--------------------------------------------------------------------------------------------------------------------
 # GRAB AND MODIFY FITS FILE
 #--------------------------------------------------------------------------------------------------------------------
-print ("INFO: Fits file grabbed for modification")
-
-fitsfile = fits.open("2MASS_FITS.fits")
-
-
-
-
-
+print ("INFO: file grabbed for modification")
 
 
 # fig = plt.figure(figsize=(10, 10), frameon=False)
@@ -235,12 +426,12 @@ def Landoltplot(image,figsize=(15,15),cmap='inferno',scale=0.5,colorbar=False,he
         cbar = plt.colorbar(im,ax=ax)
 
 
-    ax.arrow(RA[0], Dec[0]+((1/15)*Dec[0]), (RA[-1]-RA[0]), (Dec[-1]-Dec[0]),
+    ax.arrow(RA[0], Dec[0]+((1/50)*Dec[0]), (RA[-1]-RA[0]), (Dec[-1]-Dec[0]),
              head_width=0, head_length=0, 
             fc='green', ec='green', width=0.0003, 
             transform=ax.get_transform('icrs')) 
     
-    ax.arrow(RA[0], Dec[0]-((1/15)*Dec[0]), (RA[-1]-RA[0]), (Dec[-1]-Dec[0]), 
+    ax.arrow(RA[0], Dec[0]-((1/50)*Dec[0]), (RA[-1]-RA[0]), (Dec[-1]-Dec[0]), 
              head_width=0, head_length=0, 
             fc='green', ec='green', width=0.0003, 
             transform=ax.get_transform('icrs')) 
@@ -251,11 +442,11 @@ def Landoltplot(image,figsize=(15,15),cmap='inferno',scale=0.5,colorbar=False,he
     #         fc='red', ec='red', width=0.0003, 
     #         transform=ax.get_transform('icrs'))
 
-    for i in range(0,len(data)):
-        if R_Flux[i] == 0:
-            ax.scatter(RA[i],Dec[i], s=0.0001, marker=".", edgecolors=None, alpha= 0, transform=ax.get_transform('icrs'))
-        else:
-            ax.scatter(RA[i],Dec[i], s=0.0001, marker=".", edgecolors=None, transform=ax.get_transform('icrs'))
+    # for i in range(0,len(data)):
+    #     if R_Flux[i] == 0:
+    #         ax.scatter(RA[i],Dec[i], s=0.0001, marker=".", edgecolors=None, alpha= 0, transform=ax.get_transform('icrs'))
+    #     else:
+    #         ax.scatter(RA[i],Dec[i], s=0.0001, marker=".", edgecolors=None, transform=ax.get_transform('icrs'))
 
 
     
@@ -270,6 +461,5 @@ def Landoltplot(image,figsize=(15,15),cmap='inferno',scale=0.5,colorbar=False,he
 
 
 Landoltplot(image1,scale=0.5, colorbar=True,wcsplot=wcs_landolt, vmin=-4.677, vmax=10)
-
 
 print ("INFO: PNGs Created! Closing code.")
